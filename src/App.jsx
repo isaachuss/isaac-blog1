4a8f88a4c7596d7b275ea12a414c530678b82a07
isaac <ederaldohuss@gmail.com>
2025-04-25 12:13:02 -0300

    Terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>a
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className='titulo_div'>
+        <h1 className='titulo'>Isaac Blog</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='div'>
+        <h2>Isaac</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut laoreet risus. Ut est felis, hendrerit bibendum laoreet quis, rutrum et eros. Vivamus ultrices ex at vulputate hendrerit. Etiam turpis elit, aliquam nec venenatis quis, gravida volutpat nulla. Ut ullamcorper semper est, sit amet porttitor augue convallis non. Praesent fringilla non lectus sed lobortis. Nunc sagittis tellus a ante sagittis accumsan. Donec hendrerit volutpat arcu et efficitur. Cras luctus leo eu lectus egestas, et viverra nibh porttitor. Integer vel lorem id diam commodo ullamcorper.</p>
+        <h4>04/03/2009</h4>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
