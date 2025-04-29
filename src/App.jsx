e2cd69ca939bbb38fb90ca5b75bc70f28dd822b0
Isaac <ederaldohuss@gmail.com>
2025-04-29 15:41:42 -0300

    Sexto commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Post from './Components/post'
 
 
 function App() {
@@ -9,17 +10,8 @@ function App() {
 
   return (
     <>
-      <div className='titulo_div'>
-        <h1 className='titulo'>Isaac Blog</h1>
-      </div>
-      <div className='div'>
-        <div className='div_bolanome'>
-        <div className='bola'></div>
-        <h2>Isaac</h2>
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut laoreet risus. Ut est felis, hendrerit bibendum laoreet quis, rutrum et eros. Vivamus ultrices ex at vulputate hendrerit. Etiam turpis elit, aliquam nec venenatis quis, gravida volutpat nulla. Ut ullamcorper semper est, sit amet porttitor augue convallis non. Praesent fringilla non lectus sed lobortis. Nunc sagittis tellus a ante sagittis accumsan. Donec hendrerit volutpat arcu et efficitur. Cras luctus leo eu lectus egestas, et viverra nibh porttitor. Integer vel lorem id diam commodo ullamcorper.</p>
-        <h4>04/03/2009 11:30</h4>
-      </div>
+     <Post comentario={"popopopopopopo"}/>
+     <Post comentario={"pipipipipipipi"}/>
     </>
   )
 }
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Post.css'
 
 
-function Post() {
+function Post({comentario}) {
     return (
       
     <div>
@@ -14,7 +14,7 @@ function Post() {
         <div className='bola'></div>
         <h2>Isaac</h2>
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut laoreet risus. Ut est felis, hendrerit bibendum laoreet quis, rutrum et eros. Vivamus ultrices ex at vulputate hendrerit. Etiam turpis elit, aliquam nec venenatis quis, gravida volutpat nulla. Ut ullamcorper semper est, sit amet porttitor augue convallis non. Praesent fringilla non lectus sed lobortis. Nunc sagittis tellus a ante sagittis accumsan. Donec hendrerit volutpat arcu et efficitur. Cras luctus leo eu lectus egestas, et viverra nibh porttitor. Integer vel lorem id diam commodo ullamcorper.</p>
+        <p>{comentario}</p>
         <h4>04/03/2009 11:30</h4>
       </div>
       </div>
