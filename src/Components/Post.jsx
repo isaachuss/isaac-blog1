7f699a0a7bae31e87b9adec2b6ca92aa2819a367
Isaac <ederaldohuss@gmail.com>
2025-04-29 15:32:24 -0300

    Quinto commit

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './Post.css'
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className='titulo_div'>
+function Post() {
+    return (
+      
+    <div>
+        <div className='titulo_div'>
         <h1 className='titulo'>Isaac Blog</h1>
       </div>
       <div className='div'>
@@ -20,8 +17,8 @@ function App() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut laoreet risus. Ut est felis, hendrerit bibendum laoreet quis, rutrum et eros. Vivamus ultrices ex at vulputate hendrerit. Etiam turpis elit, aliquam nec venenatis quis, gravida volutpat nulla. Ut ullamcorper semper est, sit amet porttitor augue convallis non. Praesent fringilla non lectus sed lobortis. Nunc sagittis tellus a ante sagittis accumsan. Donec hendrerit volutpat arcu et efficitur. Cras luctus leo eu lectus egestas, et viverra nibh porttitor. Integer vel lorem id diam commodo ullamcorper.</p>
         <h4>04/03/2009 11:30</h4>
       </div>
-    </>
-  )
+      </div>
+      
+    )
 }
-
-export default App
+export default Post
