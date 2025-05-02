8d70cc77903242d16cf31be0e5f8e5959b783dc0
Seu Nome <seuemail@domino.com>
2025-05-02 12:19:01 -0300

    Setimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Post from './Components/post'
 
+const posts = [{ descricao: "bopopopopopopo", nome: "Isaac", data: "04/03/2009" },
+{ descricao: "bipipipipipipi", nome: "Isaac1", data: "04/03/2010" },
+{ descricao: "bupupupupupupu", nome: "Isaac2 ", data: "04/03/2011" }
+]
+const colors = ['red', 'white', 'green', 'yellow']
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contador, setContador] = useState(0)
+
 
   return (
     <>
-     <Post comentario={"popopopopopopo"}/>
-     <Post comentario={"pipipipipipipi"}/>
+      <Post comentario={"bopopopopopopo"} nome={"Isaac"} data={"04/03/2009"} cor={'yellow' [contador]} />
+      <Post comentario={"bipipipipipipi"} nome={"Isaac1"} data={"04/03/2010"} cor='green'/>
+      <Post comentario={"bupupupupupupu"} nome={"Isaac2"} data={"04/03/2011"} cor='red'/>
+
+
+      <div>
+        <p>Total de posts: {contador}</p>
+        <button onClick={() => setContador(contador + 1)}>Aumentar posts</button>
+        <button onClick={() => setContador(contador - 1)}>Diminuir posts</button>
+      </div>
+
+
     </>
   )
 }
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import './Post.css'
 
 
-function Post({comentario}) {
-    return (
-      
+function Post({ comentario, nome, data, cor }) {
+  return (
+
     <div>
-        <div className='titulo_div'>
+      <div className='titulo_div'>
         <h1 className='titulo'>Isaac Blog</h1>
       </div>
-      <div className='div'>
+      <div className='div' style={{background:cor}}>
         <div className='div_bolanome'>
-        <div className='bola'></div>
-        <h2>Isaac</h2>
+          <div className='bola'></div>
+          <h2>{nome}</h2>
         </div>
         <p>{comentario}</p>
-        <h4>04/03/2009 11:30</h4>
+        <h4>{data} 11:30</h4>
       </div>
-      </div>
-      
-    )
+    </div>
+
+  )
 }
 export default Post
